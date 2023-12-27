aa3910f3a6462616ce0839bd670a3cf54b59ba99
eua-angkoon-n <Eua-angkoon.n@jwdcoldchain.com>
2023-12-27 10:27:21 +0700

    add mysecret

---
+++ config/mysecret copy.php
@@ -0,0 +1,17 @@
+<?php
+class MySecret
+{
+    public static $conNow = 'local'; // local , db
+    public static $dbDatabase = 'db_sopongo_esp32';
+    public static $dbUser = 'xxxx';
+    public static $dbPass = 'xxxx';
+    public static $dbHost = 'xxxx';
+    public static $dbPort = 'xxxx';
+    public static $LocalDatabase = 'db_test';
+    public static $LocalUser = 'root';
+    public static $LocalPass = '';
+    public static $LocalHost = 'localhost';
+    public static $LocalPort = '';
+    public static $adminUser = 'bgadmin';
+    public static $adminPass = 'ccbgadmin@2024';
+}
